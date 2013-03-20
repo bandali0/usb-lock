149515cf6a53dbff95a23c0da9931c83ccab5434
Amin Bandali <me@aminbandali.com>
2013-03-20 12:18:00 -0400

    project files added

---
+++ onusbunplug.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+getXuser() {
+        user=`finger| grep -m1 ":$displaynum " | awk '{print $1}'`
+ 
+        if [ x"$user" = x"" ]; then
+                user=`finger| grep -m1 ":$displaynum" | awk '{print $1}'`
+        fi
+        if [ x"$user" != x"" ]; then
+                userhome=`getent passwd $user | cut -d: -f6`
+                export XAUTHORITY=$userhome/.Xauthority
+        else
+                export XAUTHORITY=""
+        fi
+}
+
+for x in /tmp/.X11-unix/*; do
+    displaynum=`echo $x | sed s#/tmp/.X11-unix/X##`
+    getXuser
+    if [ x"$XAUTHORITY" != x"" ]; then
+        # extract current state
+   export DISPLAY=":$displaynum"
+    fi
+done
+
+myresult=`pgrep .lockenabled`
+if [ -n "${myresult}" ]; then
+	su $user -c "/usr/bin/gnome-screensaver-command --lock"
+fi
